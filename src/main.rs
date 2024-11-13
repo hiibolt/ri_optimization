058969338366f6084dbef56dd9f9086afd205847
hiibolt <me@hiibolt.com>
2024-11-13 10:42:58 -0600

    feat: Ran tests

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,49 @@ use rand::distributions::{ Distribution, Uniform };
 use std::sync::{ Mutex, Arc };
 
 fn main ( ) {
-    let ( sample_size, lower_bound, upper_bound, threads ) = gen_config();
+    let ( sample_size, threads ) = gen_config();
 
     println!("[ Configuration ]");
     println!("{0: <20} {1: >10}", "Sample Size: ", sample_size );
-    println!("{0: <20} {1: >10}", "Lower Bound: ", lower_bound );
-    println!("{0: <20} {1: >10}", "Upper Bound: ", upper_bound );
     println!("{0: <20} {1: >10}", "Threads: ",     threads     );
 
     println!("\n[ Beginning Calculations ]");
 
-    // Start our clock
-    let timer = std::time::Instant::now();
+    // Create an over-arching clock
+    let global_timer = std::time::Instant::now();
 
+    // Run each digit-set
+    for digits in 2..=18 {
+        // Start our clock
+        let timer = std::time::Instant::now();
+
+        // Create our bounds
+        let upper_bound: i128 = 10i128.pow(digits);
+        let lower_bound: i128 = -upper_bound;
+
+        print!("[ Checking {digits} Digits ({0}..{1})... ", lower_bound, upper_bound);
+
+        let final_total = gen_total(
+            sample_size,
+            lower_bound,
+            upper_bound,
+            threads
+        );
+    
+        println!( " Done in {0:?}! ]", timer.elapsed());
+        println!( "{0: <35} {1: >10}", "Total Non-Positives:", final_total );
+        println!( "{0: <34} {1: >10}%\n", "% Non-Positive:", (100 * final_total ) as f64 / ( sample_size as f64 ) );
+    }
+
+    println!( "\n[ All Computations Done in {0:?}! ]", global_timer.elapsed());
+}
+
+fn gen_total (
+    sample_size: u128,
+    lower_bound: i128,
+    upper_bound: i128,
+    threads: u128
+) -> u128 {
     // Build our thread handle array and thread-safe total counter
     let mut handles = Vec::new();
     let total = Arc::new(Mutex::new(0u128));
@@ -45,20 +75,16 @@ fn main ( ) {
         }));
     }
 
-    println!("\n[ Successfully launched {threads} threads ]");
-
     for handle in handles {
         handle.join().expect("Thread join failed!");
     }
 
     let final_total = *(total.lock().expect("Failed to lock final value!"));
 
-    println!( "\n[ Done in {0:?}! ]", timer.elapsed());
-    println!( "{0: <35} {1: >10}", "Total Non-Positives:", final_total );
-    println!( "{0: <34} {1: >10}%", "% Non-Positive:", (100 * final_total ) as f64 / ( sample_size as f64 ) );
+    final_total
 }
 
-fn gen_config ( ) -> ( u128, i128, i128, u128 ) {
+fn gen_config ( ) -> ( u128, u128 ) {
     let args: Vec<String> = std::env::args()
         .collect();
 
@@ -66,18 +92,10 @@ fn gen_config ( ) -> ( u128, i128, i128, u128 ) {
         .unwrap_or(&String::from("10000000"))
         .parse::<u128>()
         .expect("First argument (sample size) must be an integer!");
-    let lower_bound = args.get(2)
-        .unwrap_or(&String::from("-10000"))
-        .parse::<i128>()
-        .expect("Second argument (lower bound) must be an integer!");
-    let upper_bound = args.get(3)
-        .unwrap_or(&String::from("10000"))
-        .parse::<i128>()
-        .expect("Third argument (upper bound) must be an integer!");
-    let threads = args.get(4)
+    let threads = args.get(2)
         .unwrap_or(&String::from("1"))
         .parse::<u128>()
-        .expect("Fourth argument (thread count) must be an unsigned integer!");
+        .expect("Second argument (thread count) must be an unsigned integer!");
 
-    ( sample_size, lower_bound, upper_bound, threads )
+    ( sample_size, threads )
 }
